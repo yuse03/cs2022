e25777ec0a24f245a33303fbbde7371685a2c5fc
yuuna seto <ys227513@univ.seitoku.ac.jp>
2022-10-03 15:18:58 +0900

    add upto stdin-1-3

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
-module Main where
+module Main where 
 
-import Lib
+showInt :: Int -> String
+showInt = show
+
+readInt :: String -> Int
+readInt = read
 
 main :: IO ()
-main = someFunc
+main = interact func
+
+-- func :: String -> String
+-- func input = unlines (map showInt [1 .. 3])
+
+-- func :: String -> String
+-- func input = showInt (27182 `mod` 818)
+
+-- func :: String -> String
+-- func input = showInt (314 * (159+265) - 358)
+
+-- func :: String -> String
+-- func input showInt (24 * 60 * 60)
+
+-- func input = showInt(2 * readInt input)
+-- func = showInt . (2 *) . readInt
+
+-- func input = case readInt input of
+--     n -> showInt (2 * n)
+
+-- func input = case readInt input of
+--     n -> showInt (n `mod` 5)
+
+func input case words input of
+    s: -> unlines [concat (replicate 3 s)]
+    [] -> error "invalid input"
